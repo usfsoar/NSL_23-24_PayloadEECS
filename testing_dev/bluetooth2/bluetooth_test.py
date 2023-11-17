1279da6590300b23dfec7932ea20d8a6d8754995
L42ARO <alvaro512@usf.edu>
2023-11-17 13:24:19 -0500

    Successful bluetooth test

---
+++ testing_dev/bluetooth2/bluetooth_test.py
@@ -0,0 +1,28 @@
+import bluetooth
+import time
+hc06_address = "FC:A8:9A:00:22:AB"
+
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+port =1
+
+try:
+	sock.connect((hc06_address, port))
+	print("Connected to HC-06")
+	sock.send("Hello there")
+	data = ""
+	while True:
+		char = sock.recv(1).decode()
+		data += char
+		if char == '\n':
+			break
+	print("Received:", data)
+except bluetooth.BluetoothError as e:
+	print("Error:", e)
+
+except Exception as e:
+	print("Error:", e)
+
+finally:
+	sock.close()
+	print("Connection closed")
