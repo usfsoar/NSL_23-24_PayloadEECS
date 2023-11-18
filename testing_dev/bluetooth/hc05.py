1d1d5d6b9a836653a18020d866a22d97738e3639
aleLovadina <alelova03@gmail.com>
2023-11-18 01:18:49 -0500

    add lora functions

--- testing_dev/bluetooth/hc05.py
+++ testing_dev/bluetooth/hc05.py
@@ -1,6 +1,6 @@
 import serial
 
-serialPort = serial.Serial(port='COM8', baudrate=9600, timeout=0, parity=serial.PARITY_EVEN, stopbits=1)
+serialPort = serial.Serial(port='COM9', baudrate=9600, timeout=0, parity=serial.PARITY_EVEN, stopbits=1)
 size = 1024
 
 while 1:
