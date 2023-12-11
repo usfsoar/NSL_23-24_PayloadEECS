e2a40d7753f8a79bb93292c98b3d59cfeb5dae16
Pavan-da-Coder <pav9362005@gmail.com>
2023-12-11 17:11:55 -0500

    Updated TransmitterCode

--- testing_dev/LoRa/TRANSMITTER_CODE/TRANSMITTER_CODE.ino
+++ testing_dev/LoRa/TRANSMITTER_CODE/TRANSMITTER_CODE.ino
@@ -60,42 +60,23 @@ Adafruit_GPS GPS(&mySerial);
 // Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
 // Set to 'true' if you want to debug and listen to the raw GPS sentences
 //Definitely consider messing with this statement to optimize code 
-#define GPSECHO  true
+#define GPSECHO  false
 
 void setup() {
-  //GPS SETUP --------------------------------------------------------------------------------------------------
-  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
-  // also spit it out
+
   Serial.begin(115200);
   delay(5000);
   Serial.println("Adafruit GPS library basic parsing test!");
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
   GPS.begin(9600);
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-  // GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data
+
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
-  // Set the update rate
+
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
-  // Request updates on antenna status, comment out to keep quiet
+
   GPS.sendCommand(PGCMD_ANTENNA);
   delay(1000);
   // Ask for firmware version
   mySerial.println(PMTK_Q_RELEASE);
-
-
-
-
-  //TRANSMITTER SETUP -----------------------------------------------------
-  //THIS IS BASED OFF OF THE TUTORIAL PROVIDED: https://projecthub.arduino.cc/mdraber/how-to-use-rylr998-lora-module-with-arduino-496504
-
-  //This tutorial calls for a baud of 57600, but the baud can be set to the maximum rate, 115200, as that is the maximum rate of the module, and the rate that the GPS examples/tutorial called
-//  Serial.begin(57600);
-
 }   
 
 uint32_t timer = millis();
@@ -103,83 +84,15 @@ uint32_t timer = millis();
 void loop() {
   //GPS SETUP --------------------------------------------------------------------------------------------------
   char c = GPS.read();
-  // if you want to debug, this is a good time to do it!
   if ((c) && (GPSECHO))
     Serial.write(c);
 
-  // if a sentence is received, we can check the checksum, parse it...
   if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
-
-    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
-      return;  // we can fail to parse a sentence in which case we should just wait for another
+    if (!GPS.parse(GPS.lastNMEA()))   
+      return;  
   }
-
   // approximately every 2 seconds or so, print out the current stats
   if (millis() - timer > 2000) {
     timer = millis(); // reset the timer0
-
-
-    //STRAYED FROM TUTORIAL: -----------------------------------------------------------------------------------------------------
-    //In the transceiver tutorial, they use Serial.println() to print the data to the "output" so the transceiver can take it
-    //The GPS tutorial/example nicely fits with this and uses Serial.print() as the main method of outputting the data, but to the monitor
-    //Make sure to use Serial.println because this prints it with a newline character, which would be the equivalent of typing out a command, and hitting enter afterwards
-    
-    // OLD -----------------------
-    Serial.println(GPS.lastNMEA());
-    Serial.println(GPS.)
-
-    //The following code will print out the data to the output. This is commented out so instead, the NMEA sentence can be parsed.
-    // Serial.print("\nTime: ");
-    // if (GPS.hour < 10) { Serial.print('0'); }
-    // Serial.print(GPS.hour, DEC); Serial.print(':');
-    // if (GPS.minute < 10) { Serial.print('0'); }
-    // Serial.print(GPS.minute, DEC); Serial.print(':');
-    // if (GPS.seconds < 10) { Serial.print('0'); }
-    // Serial.print(GPS.seconds, DEC); Serial.print('.');
-    // if (GPS.milliseconds < 10) {
-    //   Serial.print("00");
-    // } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
-    //   Serial.print("0");
-    // }
-    // Serial.println(GPS.milliseconds);
-    // Serial.print("Date: ");
-    // Serial.print(GPS.day, DEC); Serial.print('/');
-    // Serial.print(GPS.month, DEC); Serial.print("/20");
-    // Serial.println(GPS.year, DEC);
-
-    // Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    // Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-    // if (GPS.fix) {
-    //   Serial.print("Location: ");
-    //   Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-    //   Serial.print(", ");
-    //   Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-    //   Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-
-    // }
-    //   Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-    //   Serial.print("Angle: "); Serial.println(GPS.angle);
-    //   Serial.print("Altitude: "); Serial.println(GPS.altitude);
-    //   Serial.print("Antenna status: "); Serial.println((int)GPS.antenna);
-    // }
-  }
-  
-
-
-
-
-  //These are sample commands for the transceiver that I have saved for reference
-  //REYAX RYLR998 TRANSCEIVER AT COMMAND SHEET: https://reyax.com//upload/products_download/download_file/LoRa_AT_Command_RYLR998_RYLR498_EN.pdf
-  // //Transmit the data
-  // Serial.print("AT+RESET");
-  // Serial.print("AT+MODE=0");
-  // Serial.print("AT+SEND=2,1,L");
-  // Serial.print("AT+PARAMETER=");
-
-
-
-}
+    Serial.print(GPS.lastNMEA());
+}}
