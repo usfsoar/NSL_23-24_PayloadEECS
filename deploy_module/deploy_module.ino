6f9191b325ded0d2161a2119d1d6eb4025d07472
L42ARO <alvaro512@usf.edu>
2023-12-13 13:01:21 -0500

    BMP functionality added: Closes #11

--- deploy_module/deploy_module.ino
+++ deploy_module/deploy_module.ino
@@ -3,12 +3,48 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
 
 #define stepPin 10
 #define dirPin 11
 
+#define SEALEVELPRESSURE_HPA (1013.25)
+
 static const int microDelay = 900;
 static const int betweenDelay = 250;
+
+Adafruit_BMP3XX bmp;
+void bmp_setup() {
+  Wire.begin(13, 12);
+  while (!Serial);
+  Serial.println("Adafruit BMP388 / BMP390 test");
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+    //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+    //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+      Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+      Serial.println("Sensor setup error");
+      return;
+    }
+
+    // Set up oversampling and filter initialization
+    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+    bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+}
+float GetAltitude(){
+  if (!bmp.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    // Attempt to reconnect to the sensor
+    bmp_setup();
+    delay(100);
+    return 0;
+  }
+  return bmp.readAltitude(SEALEVELPRESSURE_HPA);
+}
+
 
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristic = NULL;
@@ -37,16 +73,16 @@ void moveStepper(int degrees, double vel){
 
 class Deployment{
 public:
-  Deployment()
+  Deployment(){};
   void TriggerProcedure(){
     if(!_active){
       _active = true;
       _last_checkpoint = millis();
     }
-  }
-  void StopProcedure(){
+  };
+  void Stop(){
     _active = false;
-  }
+  };
   void ProcedureCheck(){
     if(!_active) return;
     const uint32_t curr_duration = millis()-_last_checkpoint;
@@ -54,38 +90,38 @@ public:
       Serial.println("Deploying forward...");
       moveStepper(90,0.95);
     }
-    else if(!_forward && _current_duration >= _move_duration){
-      Serial.print("Stopping forward deploy");
+    else if(!_forward && curr_duration >= _move_duration){
+      Serial.println("Stopping forward deploy");
       _forward = true;
       _last_checkpoint = millis();
     }
     else if(_forward && !_nimble && curr_duration < _nimble_duration){
-      Serial.print("Allowing time to deploy...");
+      Serial.println("Allowing time to deploy...");
     }
-    else if(_forward && !_nimble && curr_duaration >= _nimble_duration){
+    else if(_forward && !_nimble && curr_duration >= _nimble_duration){
       Serial.print("Triggering retract");
       _nimble = true;
       _last_checkpoint = millis();
     }
     else if(_forward && _nimble && !_retract && curr_duration < _move_duration){
-      Serial.print("Retracting back");
+      Serial.println("Retracting back");
       moveStepper(-90,0.95);
     }
     else if(_forward && _nimble && !_retract && curr_duration > _move_duration){
-      Serial.print("Retracting completed";)
+      Serial.println("Retracting completed");
       _retract = true;
     }
     if(_forward && _nimble && _retract){
       _active = false;
     }
-  }
+  };
   void Reset(){
     _active = false;
     _forward = false;
     _nimble = false;
     _retract = false;
     _last_checkpoint = 0; 
-  }
+  };
 
 private:
   bool _started = false;
@@ -96,9 +132,8 @@ private:
   uint32_t  _last_checkpoint  =0;
   uint32_t _move_duration = 20000; //20 seconds
   uint32_t _nimble_duration = 5000; //5 seconds
-  uint32_t _current_duration = 0;
 };
-Deployment deployment();
+Deployment deployment;
 
 class MyServerCallbacks : public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -143,37 +178,29 @@ class MyCallbacks : public BLECharacteristicCallbacks {
 
 void setup() {
   Serial.begin(115200);
+  //Stepper setup------------------
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
-  // Create the BLE Device
+  //BMP setup---------------------
+  bmp_setup();
+  
+  //Bluetooth setup---------------
   BLEDevice::init("SOAR_DeployModule");
-
-  // Create the BLE Server
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
   BLEService *pService = pServer->createService("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
-
-  // Create a BLE Characteristic
   pCharacteristic = pService->createCharacteristic(
                       "6E400002-B5A3-F393-E0A9-E50E24DCCA9E",
                       BLECharacteristic::PROPERTY_NOTIFY
                     );
 
   pCharacteristic->addDescriptor(new BLE2902());
-
-  // Create another BLE Characteristic for receiving messages
   BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
           "6E400003-B5A3-F393-E0A9-E50E24DCCA9E",
           BLECharacteristic::PROPERTY_WRITE
         );
-
   pRxCharacteristic->setCallbacks(new MyCallbacks());
-
-  // Start the service
   pService->start();
-
   // Start advertising
   pServer->getAdvertising()->start();
   Serial.println("Waiting a client connection to notify...");
@@ -195,6 +222,8 @@ void loop() {
     oldDeviceConnected = deviceConnected;
   }
   deployment.ProcedureCheck();
+  // Serial.print("Altitude:");
+  // Serial.println(GetAltitude());
 }
 
 
