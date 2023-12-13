bd44fe448af0fc2e6108095285efdded3bf4cdad
L42ARO <alvaro512@usf.edu>
2023-12-13 10:51:44 -0500

    added npm to ground station

--- testing_dev/bmp3xx_simpletest/bmp3xx_simpletest.ino
+++ testing_dev/bmp3xx_simpletest/bmp3xx_simpletest.ino
@@ -1,20 +1,3 @@
-/***************************************************************************
-  This is a library for the BMP3XX temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BMP388 Breakout
-  ----> http://www.adafruit.com/products/3966
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
@@ -28,31 +11,49 @@
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BMP3XX bmp;
+uint32_t cycles = 0;
+int MCycles = 0;
 
-void setup() {
-  Serial.begin(115200);
-  Wire.begin(13,12);
+void bmp_setup() {
+  Wire.begin(13, 12);
   while (!Serial);
   Serial.println("Adafruit BMP388 / BMP390 test");
   if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
-  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
-  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
-    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
-    while (1);
-  }
+    //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+    //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+      Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+      Serial.println("Sensor setup error");
+      return;
+    }
 
-  // Set up oversampling and filter initialization
-  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+    // Set up oversampling and filter initialization
+    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+    bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+}
+
+void setup() {
+  Serial.begin(115200);
+  bmp_setup();
 }
 
 void loop() {
-  if (! bmp.performReading()) {
+  if (!bmp.performReading()) {
     Serial.println("Failed to perform reading :(");
+    MCycles = 0;
+    cycles = 0;
+    // Attempt to reconnect to the sensor
+    bmp_setup();
+
+    // Delay before next iteration
+    delay(2000);
     return;
   }
+  Serial.print("Cycles with no issue (MCycles, cycles):");
+  Serial.print(MCycles);
+  Serial.print(",");
+  Serial.println(cycles);
   Serial.print("Temperature = ");
   Serial.print(bmp.temperature);
   Serial.println(" *C");
@@ -66,5 +67,13 @@ void loop() {
   Serial.println(" m");
 
   Serial.println();
-  delay(2000);
+  cycles ++;
+  if (cycles >= UINT32_MAX - 1000) {
+    // Increase MCycles
+    MCycles++;
+    
+    // Reset cycles
+    cycles = 0;
+  }
+  delay(50);
 }
