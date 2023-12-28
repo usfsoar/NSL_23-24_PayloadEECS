164e0e630ed2b3a8e15475d672d4a640c6e368b8
L42ARO <alvaro512@usf.edu>
2023-12-28 15:22:55 -0500

    updated launcher file

--- zero_dev/launcher.sh
+++ zero_dev/launcher.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # launcher.sh
 # Echo date and time
+timestamp=$(date +"%Y%m%d%H%M%S")
+log_file="my_zero_log_$timestamp.txt"
+
 echo "Launching script at $(date)"
 
 # Navigate to the script directory
@@ -9,8 +12,8 @@ cd /home/soar/NSL_23_24_PAYLOADEECS/zero_dev
 # Save the current PID to a text file
 echo $$ > my_zero_pid.txt
 
-Execute the Python script
-sudo -E python my_zero.py --log-slave
+# Execute the Python script and append the output to the log file
+sudo -E python my_zero.py --log-slave >> "$log_file" 2>&1
 
 # Change back to the home directory or any other desired location
 cd /
