fa8a91d0fb279adeb7ea94d60e32ef783a8df108
CodingIndeed <mathewmathew628@gmail.com>
2024-02-04 19:27:10 -0500

    Done, this updated version can takes int parameter as speed.

--- testing_dev/dc-motor-test/DCMotor.h
+++ testing_dev/dc-motor-test/DCMotor.h
@@ -1,29 +1,17 @@
 class DCMotor {
- private:
+  private:
     const int pwmPin;
-    const int highDelay;
-    const int lowDelay;
+    int highDelay;
+    int lowDelay;
     bool iter;
 
  public:
     DCMotor(int pwmPin, int highDelay, int lowDelay) : pwmPin(pwmPin), highDelay(highDelay), lowDelay(lowDelay), iter(false) {}
 
-    void DC_SETUP() {
-      Serial.begin(9600); 
-      pinMode(pwmPin, OUTPUT); 
-    }
+    void DC_SETUP();
 
-    void DC_MOVE() {
-      int val = 255;
-      int del = highDelay;
-      if(iter) val = 0; 
-      if(iter) del = lowDelay; 
-      analogWrite(pwmPin, val); 
-      iter = !iter; 
-      delay(del);
-    }
+    void DC_MOVE(int speed);
 
-    void DC_STOP() {
-      analogWrite(pwmPin, 0);
-    }
+    void DC_STOP();
 };
+
--- testing_dev/dc-motor-test/dc-motor-test.ino
+++ testing_dev/dc-motor-test/dc-motor-test.ino
@@ -3,9 +3,10 @@
 DCMotor motor(9, 50, 50);
 
 void setup() {
- motor.DC_SETUP();
+  motor.DC_SETUP();
 }
 
 void loop() {
- motor.DC_MOVE();
+  int speed = 50; // replace with desired speed between -100 and 100
+  motor.DC_MOVE(speed);
 }
