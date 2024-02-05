c7e647e67fc8666cd48201c6ec52168ee7329e67
Rustambek Kholmatov <68095523+Johny-tech@users.noreply.github.com>
2024-02-05 00:33:07 -0500

    Barometer Class implementation

--- testing_dev/sensor_classes/soar_barometer.h
+++ testing_dev/sensor_classes/soar_barometer.h
@@ -1,10 +1,22 @@
+
 #ifndef SOAR_BAROMETER_h
 #define SOAR_BAROMETER_h
-
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+#define SEALEVELPRESSURE_HPA (1013.25)
+#include <Wire.h>
 class SOAR_BAROMETER {
   public:
     SOAR_BAROMETER(); // Constructor
     // Declare methods and variables here
+    void Initialize();
+     
+    float get_speed_reading();
+    float get_last_altitude_reading();
+    float * get_dataframe();
+    bool descent_check();
+    Adafruit_BMP3XX bmp;
+
   private:
     // Declare private variables and methods here
 };
