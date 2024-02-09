71d7bca3aeecf1710270ed3de2cd8d3dde2ea8c3
L42ARO <89555610+L42ARO@users.noreply.github.com>
2024-02-09 15:12:51 -0500

    added class files

---
+++ slave_module_rocket/SOAR_GPS.cpp
@@ -0,0 +1,5 @@
+#include "SOAR_GPS.h"
+
+SOAR_GPS::SOAR_GPS(){
+    Serial.print("GPS Sensor Object created");
+}
---
+++ slave_module_rocket/SOAR_GPS.h
@@ -0,0 +1,10 @@
+#ifdef SOAR_GPS_H
+#define SOAR_GPS_H
+
+class SOAR_GPS{
+    public:
+        SOAR_GPS();
+    private:
+}
+
+#endif
