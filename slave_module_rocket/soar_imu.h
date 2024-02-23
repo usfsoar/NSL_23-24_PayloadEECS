5333fc4b7ec8a56cbfe3e04af8ae85c0ab94de01
chiarad18 <111326594+chiarad18@users.noreply.github.com>
2024-02-23 18:12:20 -0500

    moved imu class and sd class files to slave_module_rocket folder

---
+++ slave_module_rocket/soar_imu.h
@@ -0,0 +1,22 @@
+#ifndef SOAR_IMU_h
+#define SOAR_IMU_h
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+
+class SOAR_IMU {
+  public:
+    SOAR_IMU(); // Constructor
+    // Declare methods and variables here
+    void BNO_SETUP();
+    float* GET_ACCELERATION();
+
+  private:
+    // Declare private variables and methods here
+    Adafruit_BNO055 bno;
+    int fail_count;
+};
+
+#endif
