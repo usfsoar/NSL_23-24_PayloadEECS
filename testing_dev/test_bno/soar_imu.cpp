a7e8560bb7a28e722b4642f5ecc1f61e30e14ed4
chiarad18 <111326594+chiarad18@users.noreply.github.com>
2024-02-28 20:07:19 -0500

    test_bno

---
+++ testing_dev/test_bno/soar_imu.cpp
@@ -0,0 +1,56 @@
+#include "SOAR_IMU.h"
+
+SOAR_IMU::SOAR_IMU() {
+  this->bno = Adafruit_BNO055(55);
+  // Constructor implementation
+  // Initialize variables if needed
+
+}
+// Implement other methods here
+
+void SOAR_IMU::BNO_SETUP() {
+
+  Serial.println("Orientation Sensor Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!this->bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+  }
+  
+  delay(1000);
+  this->bno.setExtCrystalUse(true);
+  Serial.print("Successful\n");
+}
+
+float *SOAR_IMU::GET_ACCELERATION(void) {
+
+//read imu linear acceleration vector and return an array of float
+//upon failure, call setup class from this function reset the BNO055
+
+float *acceleration = new float[3];
+
+  if(fail_count <= 20){
+    imu::Vector<3> a = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+
+    acceleration[0] = a.x();
+    acceleration[1] = a.y();
+    acceleration[0] = a.z();
+
+    for(int i=0; i<3; i++){
+      if (acceleration[i] == 0.0){
+        fail_count ++;
+      }
+      else {
+        fail_count = 0;
+      }
+    }
+  }
+  else {
+    Serial.print("fail_count > 20, resetting...");
+    this->BNO_SETUP();
+  }  
+  return acceleration;
+
+}
---
+++ testing_dev/test_bno/test_bno.ino
@@ -0,0 +1,22 @@
+#include "SOAR_IMU.h"
+
+SOAR_IMU imu_sensor;
+
+void setup() {
+    Serial.begin(115200);
+    imu_sensor.BNO_SETUP();
+    return;
+}
+
+
+void loop(){
+
+    float *a = imu_sensor.GET_ACCELERATION();
+    String out =  String(millis()) + " , " + String(a[0]) + " , " + String(a[1]) + " , " + String(a[2]) +"\n";
+    const char * c = out.c_str();
+    Serial.println(c);
+    return;
+
+}
+  
+
