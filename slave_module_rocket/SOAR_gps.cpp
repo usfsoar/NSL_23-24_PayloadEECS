cf7595c8ff709fbf32464c18867eefe23e73fe57
L42ARO <89555610+L42ARO@users.noreply.github.com>
2024-03-01 16:31:25 -0500

    gps_setup begin

---
+++ slave_module_rocket/SOAR_gps.cpp
@@ -0,0 +1,7 @@
+#include "SOAR_gps.h"
+
+SOAR_GPS::SOAR_GPS() {
+  // Constructor implementation
+  // Initialize variables if needed
+
+}
---
+++ slave_module_rocket/SOAR_gps.h
@@ -0,0 +1,12 @@
+#ifdef SOAR_GPS_h
+#define SOAR_GPS_h
+
+class SOAR_GPS {
+  public:
+    SOAR_GPS(); // Constructor
+    
+    private:
+    // Declare private variables and methods here
+};
+
+#endif
