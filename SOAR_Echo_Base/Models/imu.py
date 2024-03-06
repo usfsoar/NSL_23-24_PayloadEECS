35e085ac2930e697fb144c0add7eecd0f02634e0
YingJames <jamesy148@usf.edu>
2024-03-06 13:51:58 -0500

    add all available sensor data from BNO055

--- SOAR_Echo_Base/Models/imu.py
+++ SOAR_Echo_Base/Models/imu.py
@@ -1,19 +1,40 @@
 class IMU:
-    def __init__(self, acceleration_x=None, acceleration_y=None, acceleration_z=None,
-                 gyro_x=None, gyro_y=None, gyro_z=None, magnetic_x=None, magnetic_y=None, magnetic_z=None):
-        self.acceleration_x = acceleration_x
-        self.acceleration_y = acceleration_y
-        self.acceleration_z = acceleration_z
-        self.gyro_x = gyro_x
-        self.gyro_y = gyro_y
-        self.gyro_z = gyro_z
-        self.magnetic_x = magnetic_x
-        self.magnetic_y = magnetic_y
-        self.magnetic_z = magnetic_z
+    def __init__(
+        self,
+        euler_vector=None,
+        quaternion=None,
+        angular_velocity=None,
+        acceleration=None,
+        magnetic_field=None,
+        linear_acceleration=None,
+        gravity_vector=None,
+        temperature=None,
+    ):
+        """
+        Initialize the IMU object. All parameters are optional.
+
+        Args:
+            euler_vector (list[float], optional): The Euler angles [roll, pitch, yaw].
+            quaternion (list[float], optional): [w, x, y, z].
+            angular_velocity (list[float], optional): [x, y, z].
+            acceleration (list[float], optional): [x, y, z].
+            magnetic_field (list[float], optional: [x, y, z].
+            linear_acceleration (list[float], optional): [x, y, z].
+            gravity_vector (list[float], optional): [x, y, z].
+            temperature (float, optional): The temperature in degrees Celsius.
+        """
+        self.euler_vector = euler_vector
+        self.quaternion = quaternion
+        self.angular_velocity = angular_velocity
+        self.acceleration = acceleration
+        self.magnetic_field = magnetic_field
+        self.linear_acceleration = linear_acceleration
+        self.gravity_vector = gravity_vector
+        self.temperature = temperature
 
 # Example usage:
-# imu_data = IMU(acceleration_x=0.1, acceleration_y=0.2, acceleration_z=9.8, gyro_x=1.5, gyro_y=2.0, gyro_z=0.5)
-# imu_data.acceleration_x will give you the acceleration_x value
-# imu_data.gyro_y will give you the gyro_y value
+# imu_data = IMU(quaternion=[0.5, 0.5, 0.5, 0.5], angular_velocity=[1.0, 2.0, 3.0])
+# imu_data.angular_velocity[1] will give you the y value
+# imu_data.quaternion[0] will give you the w value
 # Similarly for other properties
 
