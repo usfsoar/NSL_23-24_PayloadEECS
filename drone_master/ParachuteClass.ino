18c4368054a060753c2d842cfa362cef1558c3ed
aleLovadina <alelova03@gmail.com>
2024-03-08 19:11:54 -0500

    added constructor to emer. trigger

--- drone_master/ParachuteClass.ino
+++ drone_master/ParachuteClass.ino
@@ -1,39 +1,47 @@
-class EmergencyTrigger {
+class EmergencyTrigger
+{
+  private:
+    float _acc_critical;
+    float _height_critical;
 
-  
 public:
   int state = 0;
-
-  void checkState();
+  
+  EmergencyTrigger(float crit_acc, float crit_height){
+    _acc_critical = crit_acc;
+    _height_critical = crit_height;
+  }
+  void checkState(float acceleration, float);
   void jettisonTrigger();
   void abortTrigger();
 };
 
-EmergencyTrigger::checkState(int state)
+void EmergencyTrigger::checkState(float acceleration, float height)
 {
-  switch (state) {
-  case 1:
-    // 
-    break;
-  case 2:
-    //
-    break;
-  default:
-    
-    break;
-}
-}
+  switch (state)
+  {
+    case 0:
+      return;
+    case 1:
+      //
+      break;
+    case 2:
+      //
+      break;
+    default:
 
-
-EmergencyTrigger::jettisonTrigger()
-{
- state = 1 
- checkState(state);
+      break;
+  }
 }
 
-EmergencyTrigger::abortTrigger()
+void EmergencyTrigger::jettisonTrigger()
 {
-  state = 2
+  state = 1;
   checkState(state);
 }
 
+void EmergencyTrigger::abortTrigger()
+{
+  state = 2;
+  checkState(state);
+}
