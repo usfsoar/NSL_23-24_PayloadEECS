2f0619fc05e9c99538cefe304668a859c0b9ee43
aleLovadina <alelova03@gmail.com>
2024-03-11 17:10:48 -0400

    divided kalman filter class and added to emerg. trigger

--- drone_master/emergency_trigger.h
+++ drone_master/emergency_trigger.h
@@ -1,18 +1,19 @@
 #ifndef EMERGENCYTRIGGER_H
 #define EMERGENCYTRIGGER_H
-#include "kalman_filter.h"
+#include "simple_kalman_filter.h"
 
 class EmergencyTrigger
 {
 private:
     float _vel_critical = 0;
     float _height_critical = 0;
-
+    KalmanFilter _kf_velocity;
+    KalmanFilter _kf_height;
 public:
     int state = 0;
 
     EmergencyTrigger(float crit_vel, float crit_height);
-    void checkState(float velocity, float height, bool low_battery = false, bool lora_response = false);
+    bool checkState(float velocity, float height, bool low_battery = false, bool lora_response = false);
     void jettisonTrigger();
     void abortTrigger();
 };
