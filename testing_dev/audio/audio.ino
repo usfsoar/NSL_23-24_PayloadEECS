faa87461747d713a0d69e3601ad4fe5fb6fcf247
aleLovadina <alelova03@gmail.com>
2024-03-29 02:26:44 -0400

    corrected audio version

--- testing_dev/audio/audio.ino
+++ testing_dev/audio/audio.ino
@@ -2,20 +2,17 @@
 //----------------------------------
 #include <Audio.h>
 #include <Wire.h>
-#include "SOAR_SD_CARD.h"
 
 // Create the audio shield object
 
 
 // Create an audio test tone, 440 Hz sine wave
 AudioSynthWaveformSine sine1;
-AudioOutputI2S2 audioOutput;  // Use AudioOutputI2S2 for secondary I2S pins
+AudioOutputI2S audioOutput;  // Use AudioOutputI2S2 for secondary I2S pins
 
 // Connect the sine wave output to the I2S output
 AudioConnection patchCord1(sine1, 0, audioOutput, 0);
 AudioConnection patchCord2(sine1, 0, audioOutput, 1);
-
-SOAR_SD_CARD sd_card(10);
 
 void setup() {
   // Enable the audio output
